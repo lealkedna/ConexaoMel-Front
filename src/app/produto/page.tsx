aa13e30512daaffe4ff35ecd9c8716e5ad5d1dcd
EmilenyRochaLeal <emilenyleal20@gmail.com>
2025-03-17 13:20:01 -0300

    alter

--- src/app/produto/page.tsx
+++ src/app/produto/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { FormProduto } from '@/components/FormProduto';
 import styles from '@/styles/Produto.module.css';
+import {api} from "@/services/api";
+import { getCookieClient } from '@/lib/cookiesClient';
 
 export default function Produto(){
+    const token = getCookieClient();
+
     return(
         <main className={styles.container}>
             <h1 className={styles.title}> Produtor  </h1>
