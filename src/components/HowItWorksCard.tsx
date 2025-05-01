865581564478f0f2b7806fd57bc53df6b7838a33
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-05-01 11:18:43 -0300

    fix: tipagem dos props em HowItWorksCard para corrigir build

--- src/components/HowItWorksCard.tsx
+++ src/components/HowItWorksCard.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import styles from "@/styles/HowItWorksCard.module.css";
 
-const HowItWorksCard = ({ icon, title, description }) => {
+interface HowItWorksCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+
+
+const HowItWorksCard = ({ icon, title, description }: HowItWorksCardProps) => {
   return (
     <div className={styles.card}>
       <div className={styles.icon}>{icon}</div>
