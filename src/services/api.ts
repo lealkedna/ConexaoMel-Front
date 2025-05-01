a4607e63650613becbaee6a9641e69517a8f9fbd
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-05-01 12:13:50 -0300

    variavel de ambiente para deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,14 @@
 // services/api.ts
 import axios from 'axios';
 
-// const api = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333',
-// });
-
-// export default api;
-
 export const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: process.env.BACKEND_URL || 'http://localhost:3333',
+    withCredentials: true,
 });
+
+
+// export const api = axios.create({
+//     baseURL: "http://localhost:3333",
+//     // https://conexao-mel.onrender.com/
+// });
 
