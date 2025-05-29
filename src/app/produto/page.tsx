b5fac155f2ee6455d813ca2d976af5ad891ccdcf
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-05-29 09:38:47 -0300

    fix: erro de deploy na pagina produto

--- src/app/produto/page.tsx
+++ src/app/produto/page.tsx
@@ -1,18 +1,20 @@
 'use client';
-import React, { useState } from 'react';
+
+// TODO: Pagina produtos é a page principal (excluir essa pagina)
+// import React, { useState } from 'react';
 import styles from '@/styles/Produto.module.css';
-import { ModalProduto } from '@/components/ModalProduto';
+// import { ModalProduto } from '@/components/ModalProduto';
 
 export default function Produto() {
-  const [modalAberto, setModalAberto] = useState(false);
+  // const [modalAberto, setModalAberto] = useState(false);
 
   return (
     <div className={styles.modalOverlay}>
         <div className={styles.modalContent}>
-            <button className={styles.closeButton} onClick={() => setModalAberto(true)}>Cadastrar produto</button>
+            {/* <button className={styles.closeButton} onClick={() => setModalAberto(true)}>Cadastrar produto</button> */}
             <h1 className={styles.title}>Produtor</h1>
             <h2>Cadastre seu Mel</h2>
-            <ModalProduto isOpen={modalAberto} onClose={() => setModalAberto(false)} />
+            {/* <ModalProduto isOpen={modalAberto} onClose={() => setModalAberto(false)} /> */}
         </div>
 
     </div>
